da9d9b8f09f58912645b560fc0ac2d80e6f98d10
Ingmar Stapel <github@stapel-ingmar.de>
2025-01-27 17:42:29 +0100

    Update install_ubuntu.sh
    The script had an error with the CUDA versions getting installed. Now it runs on my Ubuntu 24.x system with an NVIDIA A 6000 perfectly.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Autor: Ingmar Stapel
-# Date:  2024-12-23
+# Date:  2025-01-27
 # Version: 0.8
 # The script is used to setup a fresh Ubuntu installation.
 # A NVIDIA GPU is used in the system for Deep Learning activities
@@ -23,7 +23,7 @@ sudo apt-get update && sudo apt upgrade -y
 # Install necessary tools
 echo "Now we are installing a few tools we always need..."
 sleep 3
-sudo apt-get install -y mc openssh-server curl net-tools npm postgresql nodejs ffmpeg yt-dlp gnome-screenshot obs-studio gradio imagemagick
+sudo apt-get install -y mc openssh-server curl net-tools npm nodejs ffmpeg yt-dlp gnome-screenshot obs-studio imagemagick
 
 # Install NVIDIA utils
 echo "Now we are installing the NVIDIA Utils 550..."
@@ -33,37 +33,26 @@ sudo apt install -y nvidia-utils-550
 # Add CUDA repository pin
 echo "Now we are installing the NVIDIA CUDA driver..."
 sleep 3
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-ubuntu2404.pin
-sudo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update -y
+sudo apt-get -y install cuda-toolkit-12-6
 
-# Download and install the CUDA repository package
-wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda-repo-ubuntu2404-12-6-local_12.6.0-560.28.03-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2404-12-6-local_12.6.0-560.28.03-1_amd64.deb
 
-# Add CUDA keyring
-sudo cp /var/cuda-repo-ubuntu2404-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/
-
-# Update package lists after adding CUDA repository
-sudo apt-get update
-
-echo "Now we are installing the NVIDIA CUDA Toolkit..."
-sleep 3
-# Install CUDA toolkit and drivers
-sudo apt-get install -y cuda-toolkit-12-6
 sudo apt-get install -y cuda-drivers
 
 # Notify user of successful installation
-echo "Installation was completed successfully."
-
-# Prompt user for confirmation before reboot
-read -p "The system needs to reboot to apply changes. Press Enter to reboot or Ctrl+C to cancel..."
+echo "CUDA Installation was completed successfully."
+sleep 3
 
 # installing chrome
 echo "Now installing chrome as browser"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+# Prompt user for confirmation before reboot
+read -p "The system needs to reboot to apply changes. Press Enter to reboot or Ctrl+C to cancel..."
+
 # Reboot the system to apply changes
 echo "Now rebooting the system"
-sleep 3
 sudo reboot
